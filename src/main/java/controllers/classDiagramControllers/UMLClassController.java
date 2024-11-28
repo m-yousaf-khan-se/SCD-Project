106c42ddaace58eeb601dcfef1f7d10f8f649862
Muhammad Yousaf Khan <i228797@lhr.nu.edu.pk>
2024-11-28 11:30:47 +0500

    add method and add fields functionality added to the classes

--- src/main/java/controllers/classDiagramControllers/UMLClassController.java
+++ src/main/java/controllers/classDiagramControllers/UMLClassController.java
@@ -5,10 +5,16 @@ import java.util.ResourceBundle;
 
 import controllers.Controller;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 public class UMLClassController implements Controller {
 
@@ -36,13 +42,64 @@ public class UMLClassController implements Controller {
     @FXML
     private TextField variableNameField;
 
+    private ContextMenu getRightClickDeleteOption(TextField targetField) {
+        ContextMenu rightClickOptions = new ContextMenu();
+        MenuItem deleteItem = new MenuItem("Delete");
+
+        // Pass the target field directly to the action listener
+        deleteItem.setOnAction(event -> rightClickDeleteOptionListener(event, targetField));
+
+        rightClickOptions.getItems().add(deleteItem);
+
+        return rightClickOptions;
+    }
+
+    private void rightClickDeleteOptionListener(ActionEvent event, TextField targetField) {
+        if (targetField == null) {
+            System.out.println("Error: targetField is null.");
+            return;
+        }
+
+        VBox parent = (VBox) targetField.getParent();
+        if (parent != null) {
+            parent.getChildren().remove(targetField);
+            System.out.println("TextField deleted: " + targetField.getPromptText());
+        } else {
+            System.out.println("Error: Parent VBox is null.");
+        }
+    }
+
+
     @FXML
     void addNewFieldListener(ActionEvent event) {
+        int newFieldIndex = classVBox.getChildren().indexOf(addFieldBtn);  // Get index of the button
+        TextField newField = new TextField();
+
+        newField.setAlignment(Pos.CENTER);
+        newField.setFont(Font.font(10));
+        newField.setPromptText("Enter Variable Name");
+
+        // Pass the new field directly to the getRightClickDeleteOption method
+        newField.setContextMenu(getRightClickDeleteOption(newField));
+
+        classVBox.getChildren().add(newFieldIndex, newField);  // Add new field
+        System.out.println("New empty field added to the class");
     }
 
     @FXML
     void addNewMethodListener(ActionEvent event) {
+        int newFieldIndex = classVBox.getChildren().indexOf(addMethodBtn);  // Get index of the button
+        TextField newField = new TextField();
 
+        newField.setAlignment(Pos.CENTER);
+        newField.setFont(Font.font(10));
+        newField.setPromptText("Enter Method Name");
+
+        // Pass the new field directly to the getRightClickDeleteOption method
+        newField.setContextMenu(getRightClickDeleteOption(newField));
+
+        classVBox.getChildren().add(newFieldIndex, newField);  // Add new method
+        System.out.println("New empty method added to the class");
     }
 
     @FXML
@@ -54,14 +111,12 @@ public class UMLClassController implements Controller {
         assert methodNameField != null : "fx:id=\"methodNameField\" was not injected: check your FXML file 'UMLclass.fxml'.";
         assert variableNameField != null : "fx:id=\"variableNameField\" was not injected: check your FXML file 'UMLclass.fxml'.";
 
+        // Ensure context menus for predefined fields are set correctly
+        variableNameField.setContextMenu(getRightClickDeleteOption(variableNameField));
+        methodNameField.setContextMenu(getRightClickDeleteOption(methodNameField));
     }
 
 }
-
-// to add new field
-//        int newFieldindex = classVBox.getChildren().indexOf(addFieldBtn);
-//        TextField newField =
-//        classVBox.getChildren().add(newFieldindex, );
 
 
 //______________________change listener for fields:
