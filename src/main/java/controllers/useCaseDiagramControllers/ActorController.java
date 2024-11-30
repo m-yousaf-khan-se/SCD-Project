b45c1fa7167e1aa43d4e1df76961cbf755b6b80e
imranahmad88pr <125193051+imranahmad88pr@users.noreply.github.com>
2024-11-30 14:09:31 +0500

    Text fields are being tracked usecaseDiagram

--- src/main/java/controllers/useCaseDiagramControllers/ActorController.java
+++ src/main/java/controllers/useCaseDiagramControllers/ActorController.java
@@ -3,13 +3,18 @@ package controllers.useCaseDiagramControllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import controllers.Controller;
 
 public class ActorController implements Controller {
 
+
     @FXML
     private ResourceBundle resources;
+
+    @FXML
+    private TextField actorNameTextField;
 
     @FXML
     private URL location;
@@ -20,6 +25,14 @@ public class ActorController implements Controller {
     @FXML
     void initialize() {
         assert ActorPane != null : "fx:id=\"ActorPane\" was not injected: check your FXML file 'actor.fxml'.";
+
+        // Add an action listener for the TextField
+        actorNameTextField.setOnAction(event -> {
+            String inputText = actorNameTextField.getText();
+            System.out.println("Entered ActorName: " + inputText);
+
+
+        });
 
     }
 
--- src/main/java/controllers/useCaseDiagramControllers/useCaseMultiplicityController.java
+++ src/main/java/controllers/useCaseDiagramControllers/useCaseMultiplicityController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import controllers.Controller;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class useCaseMultiplicityController implements Controller {
@@ -15,12 +16,21 @@ public class useCaseMultiplicityController implements Controller {
     @FXML
     private URL location;
 
+
     @FXML
-    private Text multiplicityPane;
+    private TextField multiplicityTextField;
 
     @FXML
     void initialize() {
-        assert multiplicityPane != null : "fx:id=\"multiplicityPane\" was not injected: check your FXML file 'multiplicity.fxml'.";
+       // assert multiplicityPane != null : "fx:id=\"multiplicityPane\" was not injected: check your FXML file 'multiplicity.fxml'.";
+
+        // Add an action listener for the TextField
+        multiplicityTextField.setOnAction(event -> {
+            String inputText = multiplicityTextField.getText();
+            System.out.println("Entered Multiplicity: " + inputText);
+
+
+        });
 
     }
 
