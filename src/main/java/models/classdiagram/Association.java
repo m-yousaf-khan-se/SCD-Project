2ab1f1d0b1fe35d88c9db81c8a8221852c5c5a7c
Saad7912 <muhammadsaadtariq2003@gmail.com>
2024-12-06 00:07:12 +0500

    Connecting Model Classes Presenter Classes

--- src/main/java/models/classdiagram/Association.java
+++ src/main/java/models/classdiagram/Association.java
@@ -5,14 +5,32 @@ import models.Relationship;
 
 public class Association extends Relationship {
 
-    public Association(Component from, Component to, String label,int x, int y) {
-        super(from, to, "Association", label,x,y);
+    private String multiplicity1;
+    private String multiplicity2;
+
+    // Constructor
+    public Association(Component from, Component to, String label, int x, int y, String multiplicity1, String multiplicity2) {
+        super(from, to, "Association", label, x, y);
+        this.multiplicity1 = multiplicity1;
+        this.multiplicity2 = multiplicity2;
     }
 
-//    @Override
-//    public void display() {
-//        System.out.println("Displaying Association from " + getFrom().getDetails() + " to " + getTo().getDetails() +
-//                " with label: " + getLabel() + " at (" + getLabelX() + ", " + getLabelY() + ")");
-//    }
-}
+    // Getters and Setters
+    public String getMultiplicity1() {
+        return multiplicity1;
+    }
 
+    public void setMultiplicity1(String multiplicity1) {
+        this.multiplicity1 = multiplicity1;
+    }
+
+    public String getMultiplicity2() {
+        return multiplicity2;
+    }
+
+    public void setMultiplicity2(String multiplicity2) {
+        this.multiplicity2 = multiplicity2;
+    }
+
+
+}
